89aa4cd99c4dd6e26295d849e6c802ce91e03f8f
james hamilton <jhamilton72.jh@gmail.com>
2020-04-25 19:48:37 -0700

    final changes

---
+++ src/yourdata.js
@@ -0,0 +1,63 @@
+export default
+    {
+     
+        name :'James Hamilton',
+        headerTagline: [
+                        'Hi 👋 You found me! I am James Hamilton.',
+                       
+                        'Software Developer ',
+                       
+                        'from Arizona'
+    ],
+       
+        contactEmail:'jhamilton72.jh@gmail.com',
+        
+        abouttext: " Hi I’m James, I’m 26 years old and I’m from Morenci, Arizona. I’m currently taking this coding and web design",
+  
+       ShowAboutImage:true,
+       projects:[
+           {
+            id: 1,
+            title:'Project One', 
+             service:'Bid Pro', 
+         
+             imageSrc: "https://i.imgur.com/o8fiQsk.jpg",
+   
+             url:'https://bid-pro.herokuapp.com/'
+            },
+            {
+                id: 2,
+                title: 'Project Two',
+                service: 'Stargazerz',
+                imageSrc: "https://jhamilton20.github.io/projectone/",
+                url: 'http://chetanverma.com/'
+            },
+            { 
+                id: 3,
+                title: 'Project Three',
+                service: 'Safe520',
+                imageSrc: "https://i.imgur.com/TcsBIVu.jpg",
+                url: 'https://secure-bastion-03140.herokuapp.com/'
+            },
+            {
+                id: 4,
+                title: 'Project Four',
+                service: 'Day Planner',
+                imageSrc: "https://i.imgur.com/MntpvYb.png",
+                url: 'https://jhamilton20.github.io/homeworkfive/'
+           }
+
+                  
+        ],
+        social: [
+          
+            {   name:'Github',
+                url:'https://github.com/jhamilton20'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/jhamilton20/'
+            },
+           
+
+        ]
+    }
